6e965a2087cb44901bf8f9ef1e12e6c37da580e4
olumide-AI <olumidekolawole26@gmail.com>
2025-06-24 14:19:32 -0700

    Added constructor to the shopping cart item and implement the get all prosuct method in my dao

--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -1,26 +1,59 @@
 package org.yearup.data.mysql;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Component
 public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
 
-    public MySqlShoppingCartDao(DataSource dataSource) {
+    //Lets us get product using productId
+    private final ProductDao productDao;
+    private final DataSource dataSource;
+
+    @Autowired
+    public MySqlShoppingCartDao(DataSource dataSource, ProductDao productDao) {
         super(dataSource);
-    }
-    @Override
-    public ShoppingCart getByUserId(int userId){}
-
-    @Override
-    public void addProduct(int userId, int productId){
-
+        this.productDao = productDao;
+        this.dataSource = dataSource;
     }
 
+    @Override
+    public ShoppingCart getByUserId(int userId) {
+        String query = "SELECT product_id, quantity FROM shopping_cart WHERE user_id = ?";
+        ShoppingCart shoppingCart = new ShoppingCart();
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setInt(1, userId);
+            try (
+                    ResultSet resultSet = ps.executeQuery();
+            ) {
+                while (resultSet.next()) {
+                    int productId = resultSet.getInt("product_id");
+                    int quantity = resultSet.getInt("quantity");
+                    //How do i guard againt null pointer errors
+                    shoppingCart.add(new ShoppingCartItem(productDao.getById(productId), quantity));
+                }
+            }
 
+        }
+        catch (SQLException e){
+            System.out.println("Error in the MYSQL ShoppingCartDao: " + e.getMessage());
+        }
+        return shoppingCart;
+
+    }
+
+    @Override
 
 }
--- src/main/java/org/yearup/models/ShoppingCartItem.java
+++ src/main/java/org/yearup/models/ShoppingCartItem.java
@@ -10,6 +10,10 @@ public class ShoppingCartItem
     private int quantity = 1;
     private BigDecimal discountPercent = BigDecimal.ZERO;
 
+    public ShoppingCartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
 
     public Product getProduct()
     {
