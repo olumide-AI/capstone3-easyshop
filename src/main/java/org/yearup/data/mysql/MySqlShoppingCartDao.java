1f500a363d6d1f60b64540d5556f9d02e8e0b70c
olumide-AI <olumidekolawole26@gmail.com>
2025-06-24 15:37:22 -0700

    finished deleteitem and getting single item helper method

--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.ShoppingCartItem;
 
@@ -102,7 +103,47 @@ public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDa
 
     @Override
     public ShoppingCartItem getSingleItem(int userId, int productId){
+        String query = "SELECT quantity FROM shopping_cart WHERE user_id = ? AND product_is = ?";
+        try(
+                Connection connection = dataSource.getConnection();
+                PreparedStatement ps = connection.prepareStatement(query)
+                ){
+            ps.setInt(1, userId);
+            ps.setInt(2, productId);
+            try(
+                    ResultSet rs = ps.executeQuery()
+                    ){
+                if(rs.next()){
+                    int quantity = rs.getInt("quantity");
+                    Product product = productDao.getById(productId);
+                    return new ShoppingCartItem(product, quantity);
+                }
+            }
 
+
+        }
+
+        catch (SQLException e){
+            System.out.println("Get single cart item error: " + e.getMessage());
+
+        }
+
+        return null;
+    }
+
+    @Override
+    public void deleteItem(int userId, int productId){
+        String sql = "DELETE FROM shopping_cart WHERE user_id=? AND product_id=?";
+        try(Connection c = getConnection();
+            PreparedStatement ps = c.prepareStatement(sql)){
+            ps.setInt(1,userId);
+            ps.setInt(2,productId);
+            ps.executeUpdate();
+        }
+        catch(SQLException e){
+            System.out.println("Error deleteing a single item: " + e.getMessage());
+
+    }
     }
 
 
