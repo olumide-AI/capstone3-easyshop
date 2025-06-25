eab446c2e64161249b7df093410aefcc0cf0dad5
olumide-AI <olumidekolawole26@gmail.com>
2025-06-25 12:26:14 -0700

    profile contoller for get profile by user id

---
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -0,0 +1,58 @@
+package org.yearup.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+
+import java.security.Principal;
+
+@RestController
+@PreAuthorize("hasRole('ROLE_USER')")
+@RequestMapping("/profile")
+public class ProfileController {
+
+    private final ProfileDao profileDao;
+    private final UserDao userDao;
+
+    public ProfileController(ProfileDao profileDao, UserDao userDao) {
+        this.profileDao = profileDao;
+        this.userDao = userDao;
+    }
+
+    @GetMapping
+    public Profile getProfileByUserId(Principal principal){
+        Profile profile;
+
+        try {
+            // get the currently logged username
+
+            String userName = principal.getName();
+            // find database user by userId
+            User user = userDao.getByUserName(userName);
+            int userId = user.getId();
+
+            profile = profileDao.getProfileByUserId(userId);
+            if (profile == null){
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "profile not found");
+            }
+        } catch (ResponseStatusException e) {
+            throw e;
+        }
+        catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error getting profile ", e);
+        }
+        return profile;
+
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateProfile(@RequestBody Profile profile, Principal principal){
+        if (profile.getFirstName() == null || profile.getFirstName().isBlank())
+    }
+}
