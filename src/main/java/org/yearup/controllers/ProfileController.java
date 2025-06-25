607dce7df1a4fd4c6e63050fc09ac7087f9e40a6
olumide-AI <olumidekolawole26@gmail.com>
2025-06-25 12:31:58 -0700

    added more controller annotaions

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -1,5 +1,6 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +15,13 @@ import java.security.Principal;
 @RestController
 @PreAuthorize("hasRole('ROLE_USER')")
 @RequestMapping("/profile")
+@CrossOrigin
 public class ProfileController {
 
     private final ProfileDao profileDao;
     private final UserDao userDao;
 
+    @Autowired
     public ProfileController(ProfileDao profileDao, UserDao userDao) {
         this.profileDao = profileDao;
         this.userDao = userDao;
