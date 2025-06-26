46b416d172fe6a0b9b31fa32a659c22ef219fd42
olumide-AI <olumidekolawole26@gmail.com>
2025-06-26 16:32:29 -0700

    custom resources not found exception

---
+++ src/main/java/org/yearup/exception/CustomResourceNotFoundException.java
@@ -0,0 +1,22 @@
+package org.yearup.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class CustomResourceNotFoundException extends RuntimeException{
+    public CustomResourceNotFoundException() {
+    }
+
+    public CustomResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public CustomResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CustomResourceNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
